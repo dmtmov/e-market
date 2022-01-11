eccae6e3ffc02dd36ba2f38ab0fa007127d511f7
iren-mov <movchan.iren@gmail.com>
2022-01-11 22:53:54 +0200

    првка 1 страница

--- app/js/main.js
+++ app/js/main.js
@@ -4,10 +4,11 @@ let mixer = mixitup('.popular__inner');
 
 $(function () {
 	$('.review-slider__inner').slick({
-		prevArrow: '<button type="button" class="slick-prev slick-arrow"><svg class="reviev__slick reviev__slick--prev"><use xlink: href ="images/sprite.svg#slider-prev"></use></svg></button>',
+		prevArrow: '<button type="button" class="slick-prev slick-arrow review__slick--prev"><svg class="review__slick"><use xlink: href ="images/sprite.svg#slider-prev"></use></svg></button>',
 
 
-		nextArrow: '<button type="button" class="slick-next slick-arrow"><svg class="reviev__slick reviev__slick--next"><use xlink: href ="images/sprite.svg#slider-next"></use></svg></button>',
+		nextArrow: '<button type="button" class="slick-next slick-arrow review__slick--next"><svg class="review__slick"><use xlink: href ="images/sprite.svg#slider-next"></use></svg></button>',
+
 
 		dots: true,
 		arrows: true
