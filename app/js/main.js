db1de8341d5f5f8f151c6a0996bccf950f0651bb
iren-mov <movchan.iren@gmail.com>
2022-01-15 03:18:36 +0200

    формы 2 страница

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,6 @@
 let mixer = mixitup('.popular__inner');
 
 
-
 $(function () {
 	$('.review-slider__inner').slick({
 		prevArrow: '<button type="button" class="slick-prev slick-arrow review__slick--prev"><svg class="review__slick"><use xlink: href ="images/sprite.svg#slider-prev"></use></svg></button>',
@@ -13,4 +12,17 @@ $(function () {
 		dots: true,
 		arrows: true
 	});
+
+	$('.filter-price__input').ionRangeSlider({
+		type: "double",
+		prefix: $,
+		onChange: function (data) {
+			$('.filter-price__form').val(data.from);
+			$('.filter-price__to').val(data.to);
+		},
+
+	});
+
+	$('.cataiog-choice').styler();
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,11 @@ function scripts() {
 			'node_modules/jquery/dist/jquery.js',
 			'node_modules/mixitup/dist/mixitup.min.js',
 			'node_modules/slick-carousel/slick/slick.js',
+			'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+			'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+
 			'app/js/main.js'
+
 		])
 		.pipe(concat('main.min.js'))
 		.pipe(uglify())
@@ -161,4 +165,4 @@ exports.watching = watching;
 exports.images = images;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
-exports.default = parallel( styles, scripts,svgSprites, htmlInclude, browsersync, watching);
+exports.default = parallel(styles, scripts, svgSprites, htmlInclude, browsersync, watching);
